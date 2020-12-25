ba2a404261b5c784758f780eacb124b03d5db3bc
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2020-12-25 14:15:02 +1300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Merry xmas'));
+app.get('/', (req, res) => res.send('Merry xmas and happy new year'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
